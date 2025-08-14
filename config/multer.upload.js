bb76c0de4c6a9192cf22a4a786671fc21c2fdd73
Arpit Mahajan <arpitmahajan32@gmail.com>
2025-08-14 19:08:06 +0530

    API For Creating New Post Is Done !

--- config/multer.upload.js
+++ config/multer.upload.js
@@ -3,11 +3,34 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination : (req, file, cb) => {
-        cb(null, )
+        cb(null, 'uploads/posts');
     },
 
+    filename : (req, file, cb) => {
+        const timestamp = Date.now();
+        const originalname = file.originalname.replace(/\s+/g, '-').replace(/[^a-zA-Z0-9.-]/g, "");
+        
+        cb(null, `${timestamp}-${file.originalname}`);
+    },
 });
 
+const fileFilter = (req, file, cb) => {
+    // Accept images only and videos only
+    const allowTypes = ["image/jpeg", "image/png", "image/jpg", "image/gif", "image/svg", "video/mp4", "video/webm", "video/ogg", "audio/mp3", "audio/wav", "audio/aac", "audio/ogg"];
+
+    if(allowTypes.includes(file.mimetype)) {
+        cb(null, true);
+    }else {
+            cb(new Error("Invalid File Type"), false);
+        }
+}
+
 const postUpload = multer({
-    storage : 
+    storage : storage,
+    fileFilter : fileFilter,
+    limits : {
+        fileSize : 15 * 1024 * 1024, // 15MB
+    }
 });
+
+module.exports = postUpload;
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -3,15 +3,47 @@ const auth = require('../middlewares/auth.middleware');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const Post = require('../models/post.models');
-
+const multer = require('multer');
+const path = require('path');
+const postUpload = require('../config/multer.upload');
 const router = express.Router();
 
+
 // Create Post API Endpoint.
-router.post("/", auth, async (req, res) => {
+router.post("/", auth, postUpload.array("media", 10), async (req, res) => {
     
+    if(req.files || req.files.length === 0) {
+        return res.status(400).json({ message: "Please upload at least one media file." });
+    }
+
+    const {caption, tags, location} = req.body;
+
+    const media = req.files.map(file => {
+
+        return {
+            name : file.filename,
+            mediaType : file.mimetype.startsWith('image/')? 'image' : 'video',
+
+        }
+
+    });
+    
+    const newPost = new Post({
+        user: req.user.id,
+        caption,
+        tags,
+        location,
+        media
+    });
+
+    await newPost.save();
+
+    return res.status(201).json({message: "Post created successfully", post : newPost});
 
 });
 
+
+module.exports = router;
 
 
 
