5e2f927bb5188600f0c543f13c06deb296cbb12b
Arpit Mahajan <arpitmahajan32@gmail.com>
2025-08-14 19:46:08 +0530

    Creating API For Getting Post And Display on Screen Whenever User Open Our Application !

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -3,6 +3,7 @@ const auth = require('../middlewares/auth.middleware');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const Post = require('../models/post.models');
+const User = require('../models/user.models');
 const multer = require('multer');
 const path = require('path');
 const postUpload = require('../config/multer.upload');
@@ -55,6 +56,35 @@ router.get("/myposts", auth, async (req, res) => {
 
     const hasNextPage = posts.length === limit ? true : false;
     res.json({posts, page, limit, hasNextPage});
+
+});
+
+// API For Getting Post On Home Which Is Diplayed.
+
+router.get("/following", auth, async (req, res) => {
+    
+    let {page = 1, lmit = 10, cursor} = req.query;
+
+    page = parseInt(page);
+    limit = parseInt(limit);
+
+    const user = await User.findById(req.user._id).select('following');
+
+    let query = { user : { $in : user.following } };
+
+    if(cursor){
+        query.createdAt = { $lt : new Date(cursor)};
+    };
+
+    const posts = await Post.find(query).populate('user', '_id username profileName').sort({ createdAt: -1 }).skip((page - 1) * limit).limit(limit).lean();
+
+    const nextCursor = posts.length > 0 ? posts[posts.length - 1].createdAt : null;
+    const hasNextPage = posts.length === limit ? true : false;
+
+    
+    res.json({ posts, nextCursor, hasNextPage });
+
+
 
 });
 
